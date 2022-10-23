febb82d5f2eea78a47dfa2003ceb657a4e53686b
EvelynMCampos <evelyn.mayrac@gmail.com>
2022-10-22 22:11:47 -0300

    Adicionado função de renderizar competencias

--- js/script.js
+++ js/script.js
@@ -1,58 +1,42 @@
-console.log('Estou programando')
-const btnAdd = document.querySelector('#btn-add')
-const inputTodo = document.querySelector('#input-todo')
-const todoList = document.querySelector('#todo-list')
-
-window.addEventListener('load', ()=>{
-    renderTodoList()
-})
-
-todoList.addEventListener('click',(e)=>{
-    console.log(e)
-    if(e.target.nodeName == 'INPUT'){
-        isChecked = e.target.checked
-
-        id = e.target.getAttribute('id').replace('item-', '')
-        list = getTodoList()
-        list[id] = {name: list[id].name, checked: isChecked}
-        updateTodoList(list)
+const competencies = [
+    {
+        name: "HTML",
+        icon: "https://cdn-icons-png.flaticon.com/512/1051/1051277.png",
+    },
+    {
+        name: "CSS",
+        icon: "https://cdn-icons-png.flaticon.com/512/732/732190.png",
+    },
+    {
+        name: "JavaScript",
+        icon: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+    },
+    {
+        name: "GIT",
+        icon: "https://cdn-icons-png.flaticon.com/512/4494/4494748.png",
+    },
+    {
+        name: "GitHub",
+        icon: "https://cdn-icons-png.flaticon.com/512/5968/5968846.png",
+    },
+    {
+        name: "Python",
+        icon: "https://cdn-icons-png.flaticon.com/512/1387/1387537.png",
     }
+]
+
+const renderCompetencies = (data) => {
+    $competenciesList = document.querySelector("#competencies-list")
+    $competenciesList.innerHTML = data.map(competence => `
+            <div class="item">
+                <img src=${competence.icon} class="icon">
+                <span class="name">
+                    ${competence.name}
+                </span>
+            </div>
+    `)
+}
+
+window.addEventListener("load", () => {
+    renderCompetencies(competencies)
 })
-
-btnAdd.addEventListener('click',()=>{ 
-    inputValue = inputTodo.value
-    if(inputValue === ""){
-        alert('Digitar o nome da tarefa')
-    }
-    else{
-        addTodoElement(inputValue)
-        renderTodoList()
-    }
-
-})
-
-const clearInput = (element) => {
-    element.value = ''
-}
-
-const addTodoElement = (name) => { 
-    localStorage.setItem('todolist', JSON.stringify([...getTodoList(), {name,checked:false}]))
-}
-
-const updateTodoList = (list) => {
-    localStorage.setItem('todolist', JSON.stringify(list))
-}
-
-const getTodoList = () => localStorage.getItem('todolist') === null ? [] : JSON.parse(localStorage.getItem('todolist'))
-
-const renderTodoList = () => {
-    list = getTodoList()
-    todoList.innerHTML = list.reduce((acc,item,index) => acc + `
-    <li>
-        <input type='checkbox' id='item-${index}' ${item.checked && 'checked'}/>
-        <label for='item-${index}'> ${item.name} </label>
-        <button data-js='btn-remove-${index}'> </button>
-    </li>
-    `, '')
-}
-
